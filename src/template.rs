ebcdf1856211c5248703e73b3fd60eaf570a7f5c
David Taralla <davidtaralla@gmail.com>
2022-12-09 19:38:13 -0500

    Add template for new days

---
+++ src/template.rs
@@ -0,0 +1,9 @@
+﻿pub fn run(input: &str) -> anyhow::Result<String> {
+    for line in input.lines() {}
+    Ok("".to_string())
+}
+
+pub fn run2(input: &str) -> anyhow::Result<String> {
+    for line in input.lines() {}
+    Ok("".to_string())
+}
